59bde33b7796003c6cedb5e08316f92855c40d17
rajat1994 <rajatarora21@gmail.com>
2019-11-16 20:34:21 +0530

    Second Commit

---
+++ RemoveDuplicatesFromSortedArray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        
+        i=0
+        while i< len(nums):
+            if i!= (len(nums)-1) and nums[i]==nums[i+1]:
+                nums.pop(i+1)
+                i = i-1
+            i= i+1
+        return len(nums)
